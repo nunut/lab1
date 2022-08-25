012db65de4759fd74ddd31dbd596181569a9413d
nunut <tararesearch@hotmail.com>
2022-08-25 11:04:32 +0700

    Create CPrint.cs

---
+++ CPrint.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BasicIO
+{
+    public class BasicIOExample
+    {
+        public static void Main()
+        {
+            DateTime dat = DateTime.Now;
+
+            Console.WriteLine("Current Date and time is : "+dat);
+
+        }
+    }
+}
